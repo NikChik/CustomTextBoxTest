ac6ae404ab751533d968ae9b0cd7c5370ae8b7ca
Чикуров Никита <NikChik@list.ru>
2021-09-14 22:47:15 +0300

    CustomTextBox control was created.

---
+++ CustomTextBoxTest/Views/Controls/CustomTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomTextBoxTest.Views.Controls
+{
+    public class CustomTextBox : TextBox
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), typeof(object), typeof(CustomTextBox), new FrameworkPropertyMetadata(
+                default, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnValueChanged)));
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        public object Value
+        {
+            get => (object)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        static CustomTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomTextBox), new FrameworkPropertyMetadata(typeof(CustomTextBox)));
+        }
+    }
+}
