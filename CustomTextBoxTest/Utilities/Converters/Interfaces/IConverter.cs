39cbc4d3f5a765c418f33358d674acd4414b88fc
Чикуров Никита <NikChik@list.ru>
2021-09-14 23:26:46 +0300

    IConverter interface was added.

---
+++ CustomTextBoxTest/Utilities/Converters/Interfaces/IConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTextBoxTest.Utilities.Converters.Interfaces
+{
+    public interface IConverter<T>
+    {
+        T ParseFromString(string value);
+        string ConvertToString(T value);
+    }
+}
