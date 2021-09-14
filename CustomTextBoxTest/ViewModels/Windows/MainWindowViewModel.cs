5f4fba4050f40b42fbd8604a69048dd08ccbfbb4
Чикуров Никита <NikChik@list.ru>
2021-09-15 00:59:00 +0300

    ViewModel to MainWindow was added.

---
+++ CustomTextBoxTest/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CustomTextBoxTest.Core;
+using CustomTextBoxTest.Utilities.Converters.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTextBoxTest.ViewModels.Windows
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        private long _frequency;
+        public long Frequency
+        {
+            get => _frequency;
+            set => SetAndRaise(ref _frequency, value);
+        }
+
+        private IConverter _frequencyConverter;
+        public IConverter FrequencyConverter
+        {
+            get => _frequencyConverter;
+            set => SetAndRaise(ref _frequencyConverter, value);
+        }
+    }
+}
